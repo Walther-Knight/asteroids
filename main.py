b329eaf5957df43b47888e1ceb457401cf56e715
walther_knight <knightofargh@gmail.com>
2025-02-06 10:25:34 -0500

    add player.py and update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+import player
 
 
 
@@ -9,14 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps_clock = pygame.time.Clock()
     dt = 0
+    player_ship = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
           for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                       return
-          screen.fill(000)
+          screen.fill((0, 0, 0))
+          player_ship.draw(screen)
           pygame.display.flip()
-          dt = fps_clock.tick(60) / 1000
-    print("Starting asteroids!")
+          dt = fps_clock.tick(60) / 1000      
+    #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
 
