f74668ffc0e582f3126b87d9756b0bd072d18b8d
walther_knight <knightofargh@gmail.com>
2025-02-06 15:52:29 -0500

    update player.py to lockout shots

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
 #container support code keep commented or whole program breaks
         #if hasattr(self, "containers"):
@@ -34,11 +35,15 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        if self.shot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shot_timer = PLAYER_SHOOT_COOLDOWN
         
     def update(self, dt):
         keys = pygame.key.get_pressed()
+    # reduce shot lockout by dt
+        self.shot_timer -= dt
 
         if keys[pygame.K_a]:
             return self.rotate(-dt)
