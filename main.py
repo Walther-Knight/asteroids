53cfe2ea052753f9d09cc40887bc457151814fad
walther_knight <knightofargh@gmail.com>
2025-02-06 09:55:23 -0500

    setup game loop, add circleshape.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pygame
-import constants
+from constants import *
 
 
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    dt = 0
+    while True:
+          for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                      return
+          screen.fill(000)
+          pygame.display.flip()
+          dt = fps_clock.tick(60) / 1000
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
         main()
